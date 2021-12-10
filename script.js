40a06bd0512b7f01c59ca99e44a0e3d85c638a55
Kemal Aydık <kemalaydik@gmail.com>
2021-12-10 20:53:45 +0300

    added app class

--- script.js
+++ script.js
@@ -23,25 +23,86 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
-if (navigator.geolocation) {
-	navigator.geolocation.getCurrentPosition(
-		position => {
-			const { latitude } = position.coords;
-			const { longitude } = position.coords;
-			const coords = [latitude, longitude];
-
-			const map = L.map('map').setView(coords, 13);
-
-			L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-				attribution:
-					'&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-			}).addTo(map);
-
-			L.marker(coords)
-				.addTo(map)
-				.bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-				.openPopup();
-		},
-		() => alert('Could not retrieve position'),
-	);
+class Workout {
+	constructor(distance, duration, coords) {
+		this.distance = distance;
+		this.duration = duration;
+		this.coords = coords;
+	}
 }
+
+class Running extends Workout {
+	constructor(distance, duration, coords, cadence, pace) {
+		super(distance, duration, coords);
+		this.cadence = cadence;
+		this.pace = pace;
+	}
+}
+
+class Cycling extends Workout {
+	constructor(distance, duration, coords, elevationGain, speed) {
+		super(distance, duration, coords);
+		this.elevationGain = elevationGain;
+		this.speed = speed;
+	}
+}
+
+class App {
+	#map;
+	#mapEvent;
+	constructor() {
+		this._getPosition();
+		form.addEventListener('submit', this._newWorkOut.bind(this));
+		inputType.addEventListener('change', this._toggleElevationField.bind(this));
+	}
+	_getPosition() {
+		if (navigator.geolocation) {
+			navigator.geolocation.getCurrentPosition(this._loadMap.bind(this), () =>
+				alert('Could not retrieve position'),
+			);
+		}
+	}
+	_loadMap(position) {
+		const { latitude } = position.coords;
+		const { longitude } = position.coords;
+		const coords = [latitude, longitude];
+		this.#map = L.map('map').setView(coords, 13);
+		L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+			attribution:
+				'&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+		}).addTo(this.#map);
+		this.#map.on('click', this._showForm.bind(this));
+	}
+	_showForm(mapE) {
+		form.classList.remove('hidden');
+		inputDistance.focus();
+		this.#mapEvent = mapE;
+		console.log(this.#mapEvent);
+	}
+	_toggleElevationField() {
+		inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+		inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+	}
+	_newWorkOut(e) {
+		e.preventDefault();
+		const { lat, lng } = this.#mapEvent.latlng;
+		//clear fields
+		inputDistance.value = inputCadence.value = inputDuration.value = inputElevation.value = '';
+		//show marker
+		L.marker([lat, lng])
+			.addTo(this.#map)
+			.bindPopup(
+				L.popup({
+					maxwidt: 250,
+					minWidth: 100,
+					autoClose: false,
+					closeOnClick: false,
+					className: 'running-popup',
+				}),
+			)
+			.setPopupContent('Running')
+			.openPopup();
+	}
+}
+
+const app = new App();
